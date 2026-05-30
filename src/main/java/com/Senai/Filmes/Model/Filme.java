9198b253ac4a77d7cfb769da509cd955ce07a1fd
CesarAugustoNew <cesarprofissional22@gmail.com>
2026-05-29 21:49:54 -0300

    feat: Criação da Controller, Repository e Service

--- src/main/java/com/Senai/Filmes/Model/Filme.java
+++ src/main/java/com/Senai/Filmes/Model/Filme.java
@@ -6,8 +6,7 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import lombok.Data;
-import lombok.NonNull;
+import lombok.*;
 
 import java.util.UUID;
 
